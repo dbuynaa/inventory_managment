a17034f022b930a52a719b2388426e7785db9b87
Ben <62173996+dbuynaa@users.noreply.github.com>
2024-09-06 21:53:22 +0800

    d

--- src/env.js
+++ src/env.js
@@ -9,7 +9,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     DATABASE_PRISMA_URL: z.string().url(),
-    DATABASE_URL_NON_POOLING: z.string().url(),
+    // DATABASE_URL_NON_POOLING: z.string().url(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
       .default('development'),
@@ -41,7 +41,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING,
+    // DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING,
     DATABASE_PRISMA_URL: process.env.DATABASE_PRISMA_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
